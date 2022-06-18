8146e769e683fe72cbb7742ddd6edbaf8a8c2467
Argha Sen <iamslowdeath@gmail.com>
2022-06-19 03:27:50 +0800

    add clang format

--- src/parking_lot/parking_lot.h
+++ src/parking_lot/parking_lot.h
@@ -3,19 +3,18 @@
 #include <cstddef>
 
 namespace parkinglot {
-    class ParkingLot {
-    public:
-        ParkingLot() : ParkingLot(0) {};
+class ParkingLot {
+public:
+    ParkingLot() : ParkingLot(0){};
 
-        ParkingLot(size_t size) : m_size(size) {
+    ParkingLot(size_t size) : m_size(size) {
+    }
 
-        }
+    size_t size() {
+        return m_size;
+    }
 
-        size_t size() {
-            return m_size;
-        }
-
-    private:
-        size_t m_size;
-    };
-} // namespace parkinglot
+private:
+    size_t m_size;
+};
+}// namespace parkinglot
