3d16ed73545005562d0939abe35b9a5ddb1f25a7
Argha Sen <iamslowdeath@gmail.com>
2022-06-19 03:24:45 +0800

    add parking lot class

---
+++ src/parking_lot/parking_lot.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstddef>
+
+namespace parkinglot {
+    class ParkingLot {
+    public:
+        ParkingLot() : ParkingLot(0) {};
+
+        ParkingLot(size_t size) : m_size(size) {
+
+        }
+
+        size_t size() {
+            return m_size;
+        }
+
+    private:
+        size_t m_size;
+    };
+} // namespace parkinglot
---
+++ test/parking_lot.t.cpp
@@ -0,0 +1,16 @@
+#include "parking_lot/parking_lot.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using testing::Eq;
+
+TEST(ParkingLotTest, CanCreateEmptyParkingLot) {
+    parkinglot::ParkingLot p;
+    ASSERT_THAT(p.size(), Eq(0));
+}
+
+TEST(ParkingLotTest, CanCreateParkingLotWithSize) {
+    parkinglot::ParkingLot p(5);
+    ASSERT_THAT(p.size(), Eq(5));
+}
