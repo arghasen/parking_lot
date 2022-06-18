f60fb0200bc6f01cad13162c6b03debd62b1f426
Argha Sen <iamslowdeath@gmail.com>
2022-06-19 04:40:24 +0800

    ability to leave

--- src/parking_lot/parking_lot.h
+++ src/parking_lot/parking_lot.h
@@ -6,15 +6,23 @@
 
 namespace parkinglot {
 class ParkingLot {
-public:
-    ParkingLot(size_t size) : m_size(size) {
-        if (m_size <= 0) {
-            throw std::length_error("Invalid size specified");
-        }
-
+private:
+    void initializeSlots() {
         for (size_t i = 1; i <= m_size; ++i) {
             slots[i] = false;
         }
+    }
+    void checkSizeValidity() const {
+        if (m_size <= 0) {
+            throw std::length_error("Invalid size specified");
+        }
+    }
+
+public:
+    ParkingLot(size_t size) : m_size(size) {
+        checkSizeValidity();
+
+        initializeSlots();
     }
 
     size_t size() {
@@ -28,6 +36,18 @@ public:
                 return slot.first;
             }
         }
+        throw std::runtime_error("parking lot is full");
+    }
+
+    void leave(int slot) {
+        if (slots[slot]) {
+            slots[slot] = false;
+        } else {
+            throw std::runtime_error("cannot leave unparked spot");
+        }
+    }
+    bool checkSlotAvailability(int slotNum) {
+        return !slots.at(slotNum);
     }
 
 private:
--- test/parking_lot.t.cpp
+++ test/parking_lot.t.cpp
@@ -22,5 +22,25 @@ TEST(ParkingLotTest, CanParkWhenSpaceAvailable) {
     ASSERT_THAT(slotNo, Eq(1));
 }
 
+TEST(ParkingLotTest, ThrowsWhenSpaceNotAvailable) {
+    ParkingLot p(1);
+    (void) p.park();
+    ASSERT_THROW(p.park(), std::runtime_error);
+}
+
+TEST(ParkingLotTest, ProvidesFirstAvailableSlot) {
+    ParkingLot p(5);
+    auto slotNo = p.park();
+    slotNo = p.park();
+    ASSERT_THAT(slotNo, Eq(2));
+}
+
+TEST(ParkingLotTest, CanLeaveSlotifOccupied) {
+    ParkingLot p(5);
+    auto slotNo1 = p.park();
+    (void) p.park();
+    p.leave(slotNo1);
+    ASSERT_THAT(p.checkSlotAvailability(slotNo1), Eq(true));
+}
 }// namespace test
 }// namespace parkinglot
